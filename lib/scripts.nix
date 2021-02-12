b1a720da22d6f7080e81fd31a6d760efa10e9b9b
Peter Esselius <pepp@me.com>
2021-02-12 22:26:48 +0100

    Create /run if it does not exist

--- lib/scripts.nix
+++ lib/scripts.nix
@@ -8,6 +8,21 @@ in
   switchDarwin = writeShellScriptBin "switch-darwin" ''
     set -euo pipefail
 
+    if ! [[ -d /run ]]; then
+        if ! grep -q run /etc/synthetic.conf; then
+            echo -e 'run\tprivate/var/run' | sudo tee -a /etc/synthetic.conf
+        fi
+
+        util=/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util
+
+        # Big Sur switched to `-t`
+        if ($util -h || true) | grep -q '\-B'; then
+            $util -B
+        else
+            $util -t
+        fi
+    fi
+
     unlinkResult() {
         if [[ -L result ]]; then
             unlink result
