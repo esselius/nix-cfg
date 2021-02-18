6cfa0a5f083725ea3d568736732575d838097f2b
Peter Esselius <pepp@me.com>
2021-02-18 20:31:14 +0100

    yabai: move windows to other displays if hitting edge

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -37,10 +37,10 @@
       package = pkgs.skhd;
       skhdConfig = ''
         # move window
-        shift + cmd - h : yabai -m window --warp west
-        shift + cmd - j : yabai -m window --warp south
-        shift + cmd - k : yabai -m window --warp north
-        shift + cmd - l : yabai -m window --warp east
+        shift + cmd - h : yabai -m window --warp west  || yabai -m window --display west
+        shift + cmd - j : yabai -m window --warp south || yabai -m window --display south
+        shift + cmd - k : yabai -m window --warp north || yabai -m window --display north
+        shift + cmd - l : yabai -m window --warp east  || yabai -m window --display east
 
         # toggle split
         shift + alt - space : yabai -m window --toggle split
