4f0ab000f0dd03b037c0be6f4fa177d5891d2adc
Peter Esselius <pepp@me.com>
2021-02-23 16:49:38 +0100

    wip

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,9 +1,29 @@
-{ self, nixpkgs, ... }:
+{ self, nixpkgs, home, ... }@inputs:
 {
   nixbox = nixpkgs.lib.makeOverridable nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       ./nixos/configuration.nix
+
+      {
+        users.users.peteresselius = {
+          home = "/home/peteresselius";
+          isNormalUser = true;
+          extraGroups = ["wheel"];
+        };
+      }
+
+      {
+        imports = [ home.nixosModules.home-manager ];
+
+        home-manager.useGlobalPkgs = false;
+        home-manager.useUserPackages = false;
+        home-manager.backupFileExtension = "backup";
+
+        home-manager.extraSpecialArgs = { inherit inputs; };
+
+        home-manager.users.peteresselius = import ../../home/configurations/peteresselius;
+      }
     ];
   };
 }
